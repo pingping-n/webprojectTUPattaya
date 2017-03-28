7e2f70a458ef5d3f812393695f8037fa13de54f4
Ret3z <tuteazone111@hotmail.com>
2017-03-29 00:25:42 +0700

    page dome.php

---
+++ webtupattaya/dome1.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>TU-Pattaya</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="header-cont">
+		<div class="header">
+			<div class="left-header">
+				<img src="images/logo_tu.gif" width="70px" height="70px">
+			</div>
+			<div class="right-header">
+				มหาวิทยาลัยธรรมศาตร์<br>
+				THAMMASAT UNIVERSITY<br>
+				ศูนย์พัทยา
+			</div>
+		</div>
+		<div class="content">
+			<div class="top-content">
+				<img src="images/05_header.png" width="960" height="400">
+				<ul class="menu">
+					<ppn><a href="home.php">หน้าหลัก</a></ppn>
+					<ppn><a href="contact.php">บุคลากร</a></ppn>
+					<ppn><a href="program.php">สาขาวิชา</a></ppn>
+					<ppn><a class="active" href="dome.php">สภาพความเป็นอยู่นักศึกษา</a></ppn>
+					<ppn><a href="about.php">ผู้สนใจเข้าศึกษา</a></ppn>
+				</ul>
+			</div>
+			<div class="bottom-content">
+				<div class="column-left">
+				<ul>
+						<li><a href="dome.php">หอพักนักศึกษา</a></li>
+						<li><a href="dome1.php">สิ่งอำนวยความสะดวก</a></li>
+						<li><a href="dome2.php">อาหาร</a></li>
+				</ul>
+				</div>
+				<div class="column-right">
+					<h2>สิ่งอำนวยความสะดวก</h2>
+					<img src="images/forDome6.jpg" width="300px" height="300px">
+					<img src="images/forDome7.jpg" width="300px" height="300px">
+					<p></p>
+					<strong>ความคิดเห็นจากนักศึกษาเรื่องสิ่งอำนวยความสะดวก</strong>
+					<p></p>
+					<p>สำหรับสิ่งอำนวยความสะดวกที่นี่นั้นมีสระว่ายน้ำและห้องออกกำลังกาย เหมาะสำหรับการออกกำลังกายสำหรับผู้ที่มาเรียนที่นี่ สระว่ายเปลี่ยนน้ำอยู่ตลอดไม่ต้องห่วงเรื่องความสะอาดของน้ำ และสระมีความกว้างเหมาะสำหรับเล่นได้หลายคน ส่วนห้องออกกำลังกายมีเคร่ืองออกกำลังกายมากมายเหมาะสำหรับคนที่อยากเรียกเหงื่อ ออกกำลังกายเพ่ือเรียกกล้ามบ้าพลัง มีแอรเปิดตลอดจึงไม่ต้องห่วงเรื่องความร้อนของสภาพอากาศในห้อง</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
