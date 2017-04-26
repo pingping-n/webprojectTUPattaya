c6c1a81f62cb0dced6e2d730807a3ea888517b2b
Ret3z <tuteazone111@hotmail.com>
2017-04-26 13:19:29 +0700

    editLOGO
    edit LOGO

---
+++ about.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>TU-Pattaya</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="header-cont">
+		<div class="header">
+			<div class="left-header">
+				<img src="images/logo_tu.gif" width="100px" height="100px">
+			</div>
+			<div class="right-header">
+				มหาวิทยาลัยธรรมศาตร์<br>
+				THAMMASAT UNIVERSITY<br>
+				ศูนย์พัทยา
+			</div>
+		</div>
+		<div class="content">
+			<div class="top-content">
+				<img src="images/br.jpg" width="960" height="400">
+				<ul class="menu">
+					<ppn><a href="home.php">หน้าหลัก</a></ppn>					
+					<ppn><a href="contact.php">บุคลากร</a></ppn>
+					<ppn><a href="program.php">สาขาวิชา</a></ppn>
+					<ppn><a href="dome.php">สภาพความเป็นอยู่นักศึกษา</a></ppn>
+					<ppn><a class="active" href="about.php">ผู้สนใจเข้าศึกษา</a></ppn>
+					<ppn><a href="contractus.php">ติดต่อเรา</a></ppn>
+				</ul>
+			</div>
+			<div class="bottom-content">
+				<div class="column-left">
+					<ul>
+						<li><a href="about.php">ผู้สนใจเข้าศึกษาต่อในระดับปริญญาตรี</a></li>						
+						<li><a href="about2.php">ค่ายสำหรับนักเรียน</a></li>
+					</ul>
+				</div>
+
+				<div class="column-right">
+				<div style="overflow: scroll; height: 100%;">
+					<h2>ผู้สนใจเข้าศึกษาต่อในระกับปริญญาตรี</h2>
+					<a href="pdf\EN.pdf">1.เปิดรับสมัครนักศึกษาระดับปริญญาตรีภาควิชาวิศวกรรมซอฟแวร์</a>
+					<p>อัตราค่าธรรมเนียมการศึกษา Soft-EN</p>
+					<a href="pdf\AUTO.pdf">2.เปิดรับสมัครนักศึกษาระดับปริญญาตรีภาควิชาวิศวกรรมยานยนต์</a>
+					<p>อัตราค่าธรรมเนียมการศึกษา Auto</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
